2afb5f6af9c9751107b5c366852038bc34c0dcea
kprasad1221 <kprasad.1221@gmail.com>
2018-12-21 16:03:04 -0800

    6_PB_CU2_Lecture40 - added ground, launchpad, set up world coordinates

---
+++ Assets/Scripts/CubeAnimator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAnimator : MonoBehaviour {
+    public float animator;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Transform>().position = new Vector3 (0, Mathf.Sin(animator), 0);
+	}
+}
