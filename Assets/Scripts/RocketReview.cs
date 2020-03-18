36efc7ce9db9f9ed8dcbe2f7cc30c452d4e5ef93
kprasad1221 <kprasad.1221@gmail.com>
2020-03-17 21:52:12 -0800

    added thrust and design levers

--- Assets/Scripts/RocketReview.cs
+++ Assets/Scripts/RocketReview.cs
@@ -1,16 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RocketReview : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] float rcsThrust = 10f;
+    [SerializeField] float thrust = 10f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        Thrust();
+        Rotate();  
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float thrustSpeed = thrust * Time.deltaTime;
+            rb.AddRelativeForce(Vector3.up * thrust);
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+
+        rb.freezeRotation = true;
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+
+        rb.freezeRotation = false;
+    }
 }
